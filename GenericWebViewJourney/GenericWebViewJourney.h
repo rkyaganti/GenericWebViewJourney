c862f79891cc891b9458332422170b61059aa364
Ravi Kumar Yaganti <raviy@backbase.com>
2022-03-05 12:37:02 +0530

    Initial Project setup

---
+++ GenericWebViewJourney/GenericWebViewJourney.h
@@ -0,0 +1,18 @@
+//
+//  GenericWebViewJourney.h
+//  GenericWebViewJourney
+//
+//  Created by Ravi Yaganti on 05/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GenericWebViewJourney.
+FOUNDATION_EXPORT double GenericWebViewJourneyVersionNumber;
+
+//! Project version string for GenericWebViewJourney.
+FOUNDATION_EXPORT const unsigned char GenericWebViewJourneyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericWebViewJourney/PublicHeader.h>
+
+
---
+++ GenericWebViewJourneyExample/GenericWebViewJourneyExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericWebViewJourneyExampleApp.swift
+//  GenericWebViewJourneyExample
+//
+//  Created by Ravi Yaganti on 05/03/22.
+//
+
+import SwiftUI
+
+@main
+struct GenericWebViewJourneyExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
